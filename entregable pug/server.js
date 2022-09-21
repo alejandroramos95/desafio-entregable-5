ee76aecea4fa4ec72f524c0d42d49c5ef21e1039
Alejandro Ramos <alejandrorh295@hotmail.com>
2022-09-20 21:07:24 -0300

    desafio entregable 5

---
+++ entregable pug/server.js
@@ -0,0 +1,62 @@
+const pug = require("pug");
+const express = require("express");
+const Contenedor = require("./api/Contenedor");
+
+let productos = new Contenedor();
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+app.use(express.static("public"));
+
+const router = express.Router();
+app.use("/api", router);
+
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+
+router.get("/productos/listar", (req, res) => {
+	res.json(productos.getAll());
+});
+
+router.get("/productos/listar/:id", (req, res) => {
+	let { id } = req.params;
+	res.json(productos.getById(id));
+});
+
+router.post("/productos/guardar", (req, res) => {
+	let producto = req.body;
+	productos.save(producto);
+	res.redirect("/");
+});
+
+router.put("/productos/actualizar/:id", (req, res) => {
+	let { id } = req.params;
+	let producto = req.body;
+	productos.updateItem(producto, id);
+	res.json(producto);
+});
+
+router.delete("/productos/borrar/:id", (req, res) => {
+	let { id } = req.params;
+	let producto = productos.deleteById(id);
+	res.json(producto);
+});
+
+router.get("/productos/vista", (req, res) => {
+	let prods = productos.getAll();
+
+	res.render("layouts/index", {
+		productos: prods,
+		hayProductos: prods.length,
+	});
+});
+
+const PORT = 8080;
+
+const server = app.listen(PORT, () => {
+	console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
+});
+server.on("error", (error) => console.log(`Error en servidor ${error}`));
